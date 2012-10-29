d1ffe017bffa4a6a97f9a2cb7bb6c891beb44766
Paul <paul@email.com>
2012-10-29 16:35:18 +0800

    ...

--- utilities/Stack.c
+++ utilities/Stack.c
@@ -3,5 +3,21 @@
  * \summary {This is the implementation of Stack structure of this project.}
  */
 
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <ErrorCodes.h>
+
 struct Stack {
+  int capacity;
+  void const * * ppArray;
 };
+
+int Stack_New (struct Stack * * ppStack, int capacity) {
+  if (ppStack == NULL) return EC_NULL_POINTER;
+  if (capacity < 1) return EC_OUT_OF_RANGE;
+
+  
+
+  return EC_OK;
+}
--- utilities/Stack.h
+++ utilities/Stack.h
@@ -6,6 +6,9 @@
 #ifndef _STACK_H_
 #define _STACK_H_
 
+int Stack_New (struct Stack * * ppStack, int capacity);
+int Stack_Delete (struct Stack* pStack);
+
 int Stack_Push (struct Stack * pStack, void const * pObj);
 int Stack_Pop (struct Stack * pStack, void const * * ppObj);
 int Stack_GetTop (struct Stack * pStack, void const * * ppObj);
